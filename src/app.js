c37caa83f6dea14fea7907f08113a7c1805b1c97
benkatesh <hraj29515@gmail.com>
2026-01-16 09:25:22 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
-import CookieParser from "cookieparser"
+import cookieParser from "cookie-parser"
+
 
 
 const app=express()
@@ -12,5 +13,20 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}))// for json file that comes to the frontend 
 app.use(express.urlencoded({extended:true,limit:"16kb"}))// it encoded the url into the structure format so that server understand
 app.use(express.static("public"))//Anything inside the public folder can be shown directly to the user.”
-app.use(CookieParser())//If browser sends a cookie… please read it and put it inside req.cookies.
+app.use(cookieParser())//If browser sends a cookie… please read it and put it inside req.cookies.
+
+//routes import
+
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+
+app.use("/api/v1/users",userRouter)
+
+
+
+
+
+
+
 export { app }
